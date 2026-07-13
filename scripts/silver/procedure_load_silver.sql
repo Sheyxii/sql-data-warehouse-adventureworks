afa2d7e165d0b5423882a95b3c5e883b1d7499d3
Marciano Salazar <168376936+Sheyxii@users.noreply.github.com>
2026-07-13 23:11:16 +0800

    wip(silver): Update procedure to load data (insert) into silver tables

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -1,5 +1,6 @@
-
 -- Customer
+TRUNCATE TABLE silver.aw_customer;
+GO
 INSERT INTO silver.aw_customer (
     customer_id,
     person_id,
@@ -7,14 +8,25 @@ INSERT INTO silver.aw_customer (
     territory_id
 )
 SELECT 
-	customer_id,
+    customer_id,
     person_id,
     store_id,
     territory_id
 FROM bronze.aw_customer
 WHERE customer_id IS NOT NULL;
+GO
+
 
 -- Person
+TRUNCATE TABLE silver.aw_person;
+GO
+INSERT INTO silver.aw_person (
+    business_entity_id,
+    first_name,
+    middle_name,
+    last_name,
+    suffix
+)
 SELECT
     business_entity_id,
     NULLIF(TRIM(first_name), '') AS first_name,
@@ -24,10 +36,23 @@ SELECT
     END AS middle_name,    
     NULLIF(TRIM(last_name), '') AS last_name,
     NULLIF(TRIM(suffix), '') AS suffix
-FROM bronze.aw_person
+FROM bronze.aw_person;
+GO
 
 
 -- Product
+TRUNCATE TABLE silver.aw_product;
+GO
+INSERT INTO silver.aw_product (
+    product_id,
+    name,
+    product_number,
+    color,
+    standard_cost,
+    list_price,
+    size,
+    product_subcategory_id
+)
 SELECT
     product_id,
     TRIM(name) AS name,
@@ -38,24 +63,34 @@ SELECT
     TRIM(size) AS size,
     product_subcategory_id
 FROM bronze.aw_product;
+GO
 
 
 -- Product Subcategory
+TRUNCATE TABLE silver.aw_product_subcategory;
+GO
+INSERT INTO silver.aw_product_subcategory (
+    product_subcategory_id,
+    product_category_id,
+    name
+)
 SELECT 
     product_subcategory_id,
     product_category_id,
     TRIM(name) AS name
-FROM bronze.aw_product_subcategory
+FROM bronze.aw_product_subcategory;
+GO
 
 
 -- Product Category
+TRUNCATE TABLE silver.aw_product_category;
+GO
+INSERT INTO silver.aw_product_category (
+    product_category_id,
+    name
+)
 SELECT 
     product_category_id,
     TRIM(name) AS name
-FROM bronze.aw_product_category
-
-
-
-
-
-SELECT * FROM bronze.aw_product_subcategory
+FROM bronze.aw_product_category;
+GO
